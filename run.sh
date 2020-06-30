33eba664556e2f8021c9ea96c48c5412d2409026
Giuseppe Vietri <vietr002@umn.edu>
2020-06-29 23:47:33 -0500

    update

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python hdmm.py --dataset=adult --workload=24 --epsilon=0.1
-python hdmm.py --dataset=adult --workload=24 --epsilon=0.15
-python hdmm.py --dataset=adult --workload=24 --epsilon=0.2
-python hdmm.py --dataset=adult --workload=24 --epsilon=0.25
-python hdmm.py --dataset=adult --workload=24 --epsilon=0.5
-python hdmm.py --dataset=adult --workload=24 --epsilon=1
-python hdmm.py --dataset=adult --workload=24 --epsilon=20000
+python hdmm.py --dataset=adult --workload=60 --epsilon=0.1
+python hdmm.py --dataset=adult --workload=60 --epsilon=0.15
+python hdmm.py --dataset=adult --workload=60 --epsilon=0.2
+python hdmm.py --dataset=adult --workload=60 --epsilon=0.25
+python hdmm.py --dataset=adult --workload=60 --epsilon=0.5
+python hdmm.py --dataset=adult --workload=60 --epsilon=1
+python hdmm.py --dataset=adult --workload=60 --epsilon=20000
