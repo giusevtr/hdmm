6c82ee18812fda33cd27a9de7d9aeb6dece57b18
Giuseppe Vietri <vietr002@umn.edu>
2020-06-30 01:33:37 -0500

    update

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 wl=120
 ds="adult"
-python hdmm.py --dataset=$ds --workload=$wl --epsilon=0.1
-python hdmm.py --dataset=$ds --workload=$wl --epsilon=0.15
-python hdmm.py --dataset=$ds --workload=$wl --epsilon=0.2
-python hdmm.py --dataset=$ds --workload=$wl --epsilon=0.25
-python hdmm.py --dataset=$ds --workload=$wl --epsilon=0.5
-python hdmm.py --dataset=$ds --workload=$wl --epsilon=1
-python hdmm.py --dataset=$ds --workload=$wl --epsilon=20000
+python hdmm.py --dataset=$ds --workload=15 --epsilon=0.1
+python hdmm.py --dataset=$ds --workload=30 --epsilon=0.1
+python hdmm.py --dataset=$ds --workload=60 --epsilon=0.1
+python hdmm.py --dataset=$ds --workload=120 --epsilon=0.1
+python hdmm.py --dataset=$ds --workload=180 --epsilon=0.1
+#python hdmm.py --dataset=$ds --workload=$wl --epsilon=0.15
+#python hdmm.py --dataset=$ds --workload=$wl --epsilon=0.2
+#python hdmm.py --dataset=$ds --workload=$wl --epsilon=0.25
+#python hdmm.py --dataset=$ds --workload=$wl --epsilon=0.5
+#python hdmm.py --dataset=$ds --workload=$wl --epsilon=1
+#python hdmm.py --dataset=$ds --workload=$wl --epsilon=20000
