fbd4875dbdbaf38e11e7caf981fae7d8fea0a171
Giuseppe Vietri <vietr002@umn.edu>
2020-07-07 13:31:47 -0500

    update

--- run.sh
+++ run.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 ds="loans"
-python hdmm.py --dataset=$ds --workload=32   --marginal=3 --epsilon=0.1
-python hdmm.py --dataset=$ds --workload=64   --marginal=3 --epsilon=0.1
-python hdmm.py --dataset=$ds --workload=128  --marginal=3 --epsilon=0.1
-python hdmm.py --dataset=$ds --workload=256  --marginal=3 --epsilon=0.1
-python hdmm.py --dataset=$ds --workload=512  --marginal=3 --epsilon=0.1
-python hdmm.py --dataset=$ds --workload=1024 --marginal=3 --epsilon=0.1
+python hdmm.py --dataset=$ds --workload=64   --marginal=3 --epsilon=0.15
+python hdmm.py --dataset=$ds --workload=64   --marginal=3 --epsilon=0.2
+python hdmm.py --dataset=$ds --workload=64   --marginal=3 --epsilon=0.25
+python hdmm.py --dataset=$ds --workload=64   --marginal=3 --epsilon=0.5
+python hdmm.py --dataset=$ds --workload=64   --marginal=3 --epsilon=1.0
 
-python hdmm.py --dataset=$ds --workload=32   --marginal=5 --epsilon=0.1
-python hdmm.py --dataset=$ds --workload=64   --marginal=5 --epsilon=0.1
-python hdmm.py --dataset=$ds --workload=128  --marginal=5 --epsilon=0.1
-python hdmm.py --dataset=$ds --workload=256  --marginal=5 --epsilon=0.1
-python hdmm.py --dataset=$ds --workload=512  --marginal=5 --epsilon=0.1
-python hdmm.py --dataset=$ds --workload=1024 --marginal=5 --epsilon=0.1
+python hdmm.py --dataset=$ds --workload=64   --marginal=5 --epsilon=0.15
+python hdmm.py --dataset=$ds --workload=64   --marginal=5 --epsilon=0.2
+python hdmm.py --dataset=$ds --workload=64   --marginal=5 --epsilon=0.25
+python hdmm.py --dataset=$ds --workload=64   --marginal=5 --epsilon=0.5
+python hdmm.py --dataset=$ds --workload=64   --marginal=5 --epsilon=1.0
 
 
 ds="adult"
-python hdmm.py --dataset=$ds --workload=32   --marginal=3 --epsilon=0.1
-python hdmm.py --dataset=$ds --workload=64   --marginal=3 --epsilon=0.1
-python hdmm.py --dataset=$ds --workload=128  --marginal=3 --epsilon=0.1
-python hdmm.py --dataset=$ds --workload=256  --marginal=3 --epsilon=0.1
+python hdmm.py --dataset=$ds --workload=64   --marginal=3 --epsilon=0.15
+python hdmm.py --dataset=$ds --workload=64   --marginal=3 --epsilon=0.2
+python hdmm.py --dataset=$ds --workload=64   --marginal=3 --epsilon=0.25
+python hdmm.py --dataset=$ds --workload=64   --marginal=3 --epsilon=0.5
+python hdmm.py --dataset=$ds --workload=64   --marginal=3 --epsilon=1.0
 
-python hdmm.py --dataset=$ds --workload=32   --marginal=5 --epsilon=0.1
-python hdmm.py --dataset=$ds --workload=64   --marginal=5 --epsilon=0.1
-python hdmm.py --dataset=$ds --workload=128  --marginal=5 --epsilon=0.1
-python hdmm.py --dataset=$ds --workload=256  --marginal=5 --epsilon=0.1
-python hdmm.py --dataset=$ds --workload=512  --marginal=5 --epsilon=0.1
-python hdmm.py --dataset=$ds --workload=1024 --marginal=5 --epsilon=0.1
+python hdmm.py --dataset=$ds --workload=64   --marginal=5 --epsilon=0.15
+python hdmm.py --dataset=$ds --workload=64   --marginal=5 --epsilon=0.2
+python hdmm.py --dataset=$ds --workload=64   --marginal=5 --epsilon=0.25
+python hdmm.py --dataset=$ds --workload=64   --marginal=5 --epsilon=0.5
+python hdmm.py --dataset=$ds --workload=64   --marginal=5 --epsilon=1.0
 
